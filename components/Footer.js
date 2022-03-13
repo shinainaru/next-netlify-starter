7d53fdccfd932eec383b47df6384eaeb3ecd59ec
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-13 14:43:02 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,16 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
+      <div className={styles.footer__redes}>
+        <ul className={styles.footer__redes-wrapper}>
+          <li>
+            <a href="//youtube.com/Teokure" className={styles.footer__link}>
+              <i className={`${fab} ${fa-youtube}`}></i>
+                YouTube
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
