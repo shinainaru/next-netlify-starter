b1e2ce22d5430d6bca13dc29b814bd12e2c48b32
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-13 14:00:53 +0700

    Create get.js

---
+++ pages/get.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Why? I'am just hentai-man</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Yo! You get anime hentai" />
+        Setiap hari kerjaannya hanya coli. Sampe kontolnya sakit
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
