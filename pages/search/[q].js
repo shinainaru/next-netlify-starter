76ef305287fa750a27f8b1f42d694ff433a2f963
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 03:46:43 +0700

    Update [q].js

--- pages/search/[q].js
+++ pages/search/[q].js
@@ -2,20 +2,10 @@ import axios from "axios"
 import Head from "next/head"
 import Header from '@components/Header'
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
 
-function Search() {
+function Search({ response }) {
   const router = useRouter()
   const { q } = router.query
-  const [anime, setAnime] = useState([])
-  useEffect(() => {
-    const fetchAnime = async (mal_id) => {
-      const response = await searchAnime(mal_id)
-      setAnime(response)
-    }
-
-    fetchAnime(router.query.q)
-  }, [router.query.q])
 
   return (
     <>
@@ -23,21 +13,20 @@ function Search() {
         <title>{q} · Mr.Cyser#R00t</title>
       </Head>
       <div className="max-w-full">
-        <h1>{anime}</h1>
+        <h1>{response}</h1>
       </div>
     </>
   )
 }
 
-const searchAnime = async (query) => {
-  try {
-    const request = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}`)
-    const response = request.data
+export async function getStaticProps() {
+  const request = await axios.get(`https://api.jikan.moe/v4/anime?q=${router.query.q}`)
+  const response = request.data
 
-    return response
-  } catch (error) {
-    console.error(error)
+  return {
+    props: { response }
   }
 }
+
 
 export default Search
