ca32f4818c77b97816d11da1e1b3b769da0ddac0
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-13 17:06:22 +0700

    Update [slug].js

--- pages/search/[slug].js
+++ pages/search/[slug].js
@@ -4,8 +4,10 @@ import Head from 'next/head'
 export default function Search() {
   const { asPath } = useRouter();
   return (
-    <Head>
-      <title> Hasil Pencarian {asPath} - angga-buat-web.netlify.app </title>
-    <Head />
+    <div className="container">
+      <Head>
+        <title> Hasil Pencarian {asPath} - angga-buat-web.netlify.app </title>
+      <Head />
+    </div>
   )
 }
