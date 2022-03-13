2d59c1ab37daddacc6f9e78e123ac37699840a5a
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-13 17:21:44 +0700

    Update [slug].js

--- pages/search/[slug].js
+++ pages/search/[slug].js
@@ -1,15 +1,23 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head'
+import Header from '@components/Header'
 
 const search = () => {
-  const { asPath, pathname } = useRouter();
+  const { asPath, pathname } = useRouter()
   return (
-    <>
+  <div className="container>
       <Head>
         <title>Hasil Pencarian {asPath} </title>
       <Head />
-    </>
-  );
+
+      <main>
+        <Header title="Hasil Pencarian {asPath}" />
+        <p className="description">
+          GOOD<code>pages/search/index.js</code>
+        </p>
+      </main>
+    </div>
+  )
 }
 
-export default search;
+export default search
