cf5a0d1c011bbba97508afa04dc3c0b387f8e479
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 03:36:33 +0700

    Update [q].js

--- pages/search/[q].js
+++ pages/search/[q].js
@@ -1,18 +1,43 @@
+import axios from "axios"
 import Head from "next/head"
 import Header from '@components/Header'
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
 function Search() {
   const router = useRouter()
   const { q } = router.query
+  const [anime, setAnime] = useState([])
+  useEffect(() => {
+    const fetchAnime = async (mal_id) => {
+      const response = await searchAnime(mal_id)
+      setAnime(response)
+    }
+
+    fetchAnime(router.query.q)
+  }, [router.query.q])
+
   return (
     <>
       <Head>
         <title>{q} · Mr.Cyser#R00t</title>
       </Head>
-      <Header title="tch" />
+      <div className="max-w-full">
+        <h1>{anime}</h1>
+      </div>
     </>
   )
+}
+
+const searchAnime = async (query) => {
+  try {
+    const request = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}`)
+    const response = request.data
+
+    return response
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export default Search
