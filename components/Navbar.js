f76e296831ea680ed1e09ae92955b9234b68c950
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 18:49:45 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,15 +13,17 @@ export default function Navbar() {
   const GenerateNav = ({ value }) => {
     return (
       <>
-      {navigation.map(nav => (
-        <li key={nav.text}>
-          <Link href={nav.link}>
-            <a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-              {nav.text}
-            </a>
-          </Link>
-        </li>
-      ))}
+        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+          {navigation.map(nav => (
+            <li key={nav.text}>
+              <Link href={nav.link}>
+                <a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  {nav.text}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </>
     );
   }
