ccef618286841d36988f2ce9817d7f0b08cf0b0a
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 16:38:07 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -30,15 +30,14 @@ export default function Navbar() {
             <div class="w-full md:block md:w-auto" id="mobile-menu">
             <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               <li>
-                <a href="#"
+                <a href="/"
                    class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Check
+                  {showNav ? 'Check' : 'NO'}
                 </a>
               </li>
             </ul>
           </div>
-          ))}
         </div>
       </nav>
     </>
