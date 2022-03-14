0e6517421a9d92b2625987b4649b250b58dfbd47
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 18:44:22 +0700

    A

--- components/Navbar.js
+++ components/Navbar.js
@@ -43,10 +43,10 @@ export default function Navbar() {
             {showNav ? <GenerateNav value={navigation} />: <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               {navigation.map(nav => (
                 <li key={nav.text}>
-                  <Link href={nav.link}
-                    class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  <Link href={nav.link}>
+                    <a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                  {nav.text}
+                  {nav.text}</a>
                   </Link>
                 </li>
               ))}
