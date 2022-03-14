2c03917380cbc8f66d57d8d4220e93e01ed3c012
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 15:08:14 +0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,10 @@
 import Footer from "./Footer"
+import Navbar from "./Navbar"
 
 function Layout({ children }) {
   return (
     <div className="min-h-screen bg-slate-900 text-white">
+      <Navbar />
       <div className="max-w-7xl p-4 xl:m-auto">
         <main>{children}</main>
       </div>
