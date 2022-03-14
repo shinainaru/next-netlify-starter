9d7d94325326343e12643411b1df3d411b01c5bd
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 02:15:21 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -48,7 +48,8 @@ export default function Navbar() {
               {navigation.map(nav => (
                 <li key={nav.text}>
                   <Link href={nav.link}>
-                    <a class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    <a onBlur={handleToggle} onFocus={handleToggle}
+                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                   {nav.text}</a>
                   </Link>
