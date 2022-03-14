3519a2ccbc10d525d834937fd58e83199c24cb10
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 17:59:45 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -29,14 +29,15 @@ export default function Navbar() {
             {showNav ? <h1>Tolong Buka di HP. </h1> : <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               {navigation.map(nav => (
                 <li key={nav.text}>
-                  <a href={nav.link}
+                  <Link href={nav.link}
                     class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                   {nav.text}
-                  </a>
+                  </Link>
                 </li>
               ))}
             </ul>
+            }
           </div>
         </div>
       </nav>
