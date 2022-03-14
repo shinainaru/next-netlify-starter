4cbe9f1dccc521eb0b583569ccdc6cfcebd57da1
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-14 17:01:55 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
             <div class="w-full md:block md:w-auto" id="mobile-menu">
             {!showNav ? <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               {navigation.map(nav => (
-                <li key=nav.text>
+                <li key={nav.text}>
                   <a href={nav.link}
                      class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
