95b0e31a49e98581bbbb3c9dbc6aa744adcd8997
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 12:08:57 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,12 @@ import '@styles/globals.css'
 import App from 'next/app'
 import Footer from '@components/Footer'
 import Layout from '@components/Layout'
-import Head from 'next/head'
 
 export default class Application extends App {
   render () {
     const { Component, pageProps } = this.props
     return (
       <>
-        <Head>
-          <meta name="theme-color" content="#374151">
-        </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
