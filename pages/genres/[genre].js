d583c10fbabc7b47f0c25b5201a84bf7225b98e5
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 14:51:29 +0700

    Update [genre].js

--- pages/genres/[genre].js
+++ pages/genres/[genre].js
@@ -5,8 +5,8 @@ import NotFound from "@components/NotFound"
 function GetGenre() {
   const router = useRouter()
   const genreList = ['anal', 'milf']
-  const { genre } = router.quer
-  const found = genre ? genre.toLowerCase().includes(genreList) ? <h1> FOUND </h1> : <NotFound /> : ''
+  const { genre } = router.query
+  const found = genre && genre.toLowerCase().includes(genreList) ? <h1> FOUND </h1> : <NotFound /> : ''
   return (
     <>
       {found}
