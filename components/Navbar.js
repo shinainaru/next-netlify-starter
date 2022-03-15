b9e42e316efac08cca296dc030a0abf62313b6c3
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 17:16:14 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -45,8 +45,8 @@ export default function Navbar() {
       <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-gray-800 dark:bg-gray-800">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
           <a href="#" class="flex items-center">
-            <img src="/logo.jpg" class="mr-3 h-6 sm:h-10" alt="Flowbite Logo" />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">ThisEM</span>
+            <img src="/logo.jpg" class="mr-3 h-6 sm:h-10" alt="Kinda SUS logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">I'am SUS</span>
           </a>
           <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false" onClick={handleToggle}>
             <span class="sr-only">Open main menu</span>
