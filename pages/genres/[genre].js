cdd2ede0ac01946094b87167a1ff70835931c403
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 14:33:37 +0700

    Update [genre].js

--- pages/genres/[genre].js
+++ pages/genres/[genre].js
@@ -1,12 +1,11 @@
 import Head from "next/head"
 import { useRouter } from "next/router"
-import Custom404 from "@pages/404"
 
 function Search({ response }) {
   const router = useRouter()
   const genreList = ['anal', 'milf']
   const { genre } = router.query
-  const found = genre.toLowerCase().includes(genreList) ? <h1> FOUND </h1> : <Custom404 />
+  const found = genre.toLowerCase().includes(genreList) ? <h1> FOUND </h1> : router.push('/404')
   return (
     <>
       {found}
