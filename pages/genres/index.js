6f5230d4e830dcbfe10eee7b0e0f0f1deb4f87c6
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 12:55:39 +0700

    Update index.js

--- pages/genres/index.js
+++ pages/genres/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head"
+import Link from "next/link"
 
 export default function Genres() {
+  const genre = [
+    { link: '/genres/milf', text: 'Milf'},
+    { link: '/genres/anal', text: 'Anal' },
+  ];
   return (
     <>
       <Head>
@@ -8,11 +13,15 @@ export default function Genres() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="max-w-full">
-        <div className="mt-2 grid w-1/2 grid-cols-15 gap-5 sm:grid-cols-7 md:grid-cols-15 md:gap-3 lg:grid-cols-7">
-          <div className="group cursor-pointer overflow-hidden rounded-lg border-b-2 border-b-transparent bg-slate-800 hover:border-b-rose-700">
-            <h2 className="truncate p-2 break-all">Milf</h2>
+        {genre.map(gen => (
+          <div className="mt-2 grid w-1/2 grid-cols-15 gap-5 sm:grid-cols-7 md:grid-cols-15 md:gap-3 lg:grid-cols-7">
+            <div className="group cursor-pointer overflow-hidden rounded-lg border-b-2 border-b-transparent bg-slate-800 hover:border-b-rose-700">
+              <Link href={gen.link}>
+                <a className="truncate p-2 break-all">{gen.text}</a>
+              </Link>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   )
