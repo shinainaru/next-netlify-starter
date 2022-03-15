0b90d6511df69139df93e88e4803831a1cd9dbed
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 12:11:37 +0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,21 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import Head from "next/head"
 
 function Layout({ children }) {
   return (
-    <div className="min-h-screen bg-slate-900 text-white">
-      <Navbar />
-      <div className="max-w-7xl p-4 xl:m-auto">
-        <main>{children}</main>
+    <>
+      <Head>
+        <meta name="theme-color" content="#374151" />
+      </Head>
+      <div className="min-h-screen bg-slate-900 text-white">
+        <Navbar />
+          <div className="max-w-7xl p-4 xl:m-auto">
+            <main>{children}</main>
+          </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
 
