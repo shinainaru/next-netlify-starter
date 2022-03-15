e7affd2aec70181081a73479a983e1fc079cf80a
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 12:40:59 +0700

    Update index.js

--- pages/genres/index.js
+++ pages/genres/index.js
@@ -8,7 +8,7 @@ export default function Genres() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="max-w-full">
-        <div className="mt-2 grid w-full grid-cols-10 gap-5 sm:grid-cols-20 md:grid-cols-13 md:gap-3 lg:grid-cols-7">
+        <div className="mt-2 grid w-full grid-cols-15 gap-5 sm:grid-cols-10 md:grid-cols-15 md:gap-3 lg:grid-cols-7">
           <div className="group cursor-pointer overflow-hidden rounded-lg border-b-2 border-b-transparent bg-slate-800 hover:border-b-rose-700">
             <h2 className="truncate p-2 break-all">Milf</h2>
           </div>
