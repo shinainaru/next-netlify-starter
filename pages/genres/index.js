68a928d06c217fa81854602bc444cc90d40e9590
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-15 12:58:49 +0700

    Update index.js

--- pages/genres/index.js
+++ pages/genres/index.js
@@ -14,7 +14,7 @@ export default function Genres() {
       </Head>
       <div className="max-w-full">
         {genre.map(gen => (
-          <div className="mt-2 grid w-1/2 grid-cols-15 gap-5 sm:grid-cols-7 md:grid-cols-15 md:gap-3 lg:grid-cols-7">
+          <div className="mt-2 grid w-1/2 grid-cols-2 gap-5 sm:grid-cols-7 md:grid-cols-15 md:gap-3 lg:grid-cols-7">
             <div className="group cursor-pointer overflow-hidden rounded-lg border-b-2 border-b-transparent bg-slate-800 hover:border-b-rose-700">
               <Link href={gen.link}>
                 <a className="truncate p-2 break-all">{gen.text}</a>
