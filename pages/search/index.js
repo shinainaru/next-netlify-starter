780a3a76b3c1ac20fbf2bda08834f1c0fbd4e96d
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-16 17:23:12 +0700

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,25 +1,27 @@
-import Link from "next/link"
+import { useRouter } from "next/router"
 import {useEffect, useState} from "react";
 
 function IndexSearch() {
+  const router = useRouter()
   const [query, setQuery] = useState(null)
   useEffect(() => {
     setQuery(document.getElementById('search').value)
   }, [])
+  const goo = () => {
+    router.push('/search/' + query)
+  }
   return (
     <>
       <div class="flex items-center justify-center">
         <div class="flex border-2 rounded">
           <input type="text" id="search" class="px-4 py-2 w-80" placeholder="Search..." />
-          <Link href={`/search/${query}`} passHref>
-            <button class="flex items-center justify-center px-4 border-l">
-                <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24">
-                    <path
-                        d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-                </svg>
-            </button>
-          </Link>
+          <button onClick={goo()} class="flex items-center justify-center px-4 border-l">
+              <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24">
+                  <path
+                      d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+              </svg>
+          </button>
         </div>
       </div>
     </>
