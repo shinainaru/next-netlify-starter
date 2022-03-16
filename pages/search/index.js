21685d34d678161932ea51791186cde38a1052b7
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-16 14:00:21 +0700

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,17 +1,41 @@
 import { useRouter } from "next/router"
 
-function IndexSearch() {
-  const { asPath, query, push } = useRouter()
-  if (asPath.match("/search") && query && query.q) {
-    push('/search/' + query.q)
-  }
-  
+function IndexSearch({ query }) {
   return (
     <>
-      <h3>Error 403</h3>
-      <p>Kamu akan di alihkan ke halaman /search/QUERY </p>
+      {query && (
+      <div class="flex items-center justify-center">
+        <div class="flex border-2 rounded">
+          <form>
+            <input type="text" class="px-4 py-2 w-80" placeholder="Search..." />
+            <button class="flex items-center justify-center px-4 border-l" type="submit">
+              <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24">
+                  <path
+                      d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+              </svg>
+            </button>
+          </form>
+        </div>
+      </div>
+      )}
     </>
   )
 }
 
 export default IndexSearch
+
+export async function getServerSideProps(context) {
+
+  const { asPath, query, push } = context
+  const arah = asPath.match("/search") && query && query.q
+  if (arah) {
+    context.res.setHeader('Location', '/search/' + query.q)
+  }
+
+  return {
+    props: {
+      query: arah
+    }
+  }
+}
