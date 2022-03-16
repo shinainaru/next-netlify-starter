fc0047ed1aa52e0e6208650d4bc8d2d8017a5c40
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-16 18:07:28 +0700

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -6,16 +6,16 @@ function IndexSearch() {
   const router = useRouter()
   return (
     <>
-      <div class="container flex flex-wrap items-center justify-center bg-slate-900 mx-auto">
-        <div class="flex border-2 rounded border-gray-800 bg-gray-900">
-          <input type="text" onKeyDown={() => router.push('/search/' + query)} onChange={(e) => setQuery(e.target.value)} class="px-4 py-2 w-80 sm:w-50 text-gray-800" placeholder="Search..." />
-          <button onClick={() => router.push('/search/' + query)} class="flex items-center justify-center px-4 border-l">
-              <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24">
-                  <path
-                      d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+      <div class="flex justify-center">
+        <div class="mb-3 xl:w-96">
+          <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
+            <input type="search" onKeyDown={() => router.push('/search/' + query)} onChange={(e) => setQuery(e.target.value)} class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2" />
+            <button onClick={() => router.push('/search/' + query)} class="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
               </svg>
-          </button>
+            </button>
+          </div>
         </div>
       </div>
     </>
