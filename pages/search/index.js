d881be38d57a27667127d1b5776a26b2f5bd13a9
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-16 13:16:32 +0700

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,22 +2,12 @@ import { useRouter } from "next/router"
 
 function IndexSearch() {
   const { pathname, query } = useRouter()
+  
   return (
     <>
-    <h1>Path: {pathname} Query: {query}</h1>
-    <div class="flex items-center justify-center">
-      <div class="flex border-2 rounded">
-          <input type="text" class="px-4 py-2 w-80" placeholder="Search..." />
-          <button class="flex items-center justify-center px-4 border-l">
-              <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24">
-                  <path
-                      d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-              </svg>
-          </button>
-      </div>
-    </div>
+      <h1>Path: {pathname} Query: {query}</h1>
     </>
   )
 }
+
 export default IndexSearch
