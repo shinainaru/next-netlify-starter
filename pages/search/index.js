d22bf3fa68e83aa0932cafd73637abcbe9ece3fa
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-16 19:39:13 +0700

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -7,7 +7,7 @@ function IndexSearch() {
   return (
     <>
       <div class="flex border-grey-light border">
-	<input onKeyDown={() => router.push('/search/' + query)} onChange={(e) => setQuery(e.target.value)} class="w-full rounded ml-1" type="text" placeholder="Search...">
+	<input onKeyDown={() => router.push('/search/' + query)} onChange={(e) => setQuery(e.target.value)} class="w-full rounded ml-1" type="search" placeholder="Search..." />
 	<button onClick={() => router.push('/search/' + query)} class="bg-grey-lightest border-grey border-l shadow hover:bg-grey-lightest">
 	  <span class="w-auto flex justify-end items-center text-grey p-2 hover:text-grey-darkest">
             <i class="material-icons text-xl">search</i>
