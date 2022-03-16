f7345c47d8e10bf2837fa62c7064d9e000891611
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-16 18:24:13 +0700

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -6,7 +6,7 @@ function IndexSearch() {
   const router = useRouter()
   return (
     <>
-      <div class="flex justify-center">
+      <div class="flex justify-center items-center">
         <div class="mb-3 xl:w-96">
           <div class="input-group relative flex flex-wrap items-stretch w-70 mb-4">
             <input type="search" onKeyDown={() => router.push('/search/' + query)} onChange={(e) => setQuery(e.target.value)} class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2" />
