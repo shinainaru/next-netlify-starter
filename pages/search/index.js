a248f799c595290d805df7d786d4256f0a8cf997
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-03-16 13:38:19 +0700

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,8 +2,11 @@ import { useRouter } from "next/router"
 
 function IndexSearch() {
   const { asPath, query } = useRouter()
-  console.log(query)
-  console.log(asPath)
+  setTimeout(() => {
+    console.log(query)
+    console.log(asPath)
+  }, 1000 * 60 * 1.3)
+  
   return (
     <>
       <h1>You get RickRolled</h1>
