5ff9eca70dd7405d9ef810db5b6428037d59cafe
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-04-01 09:45:21 +0700

    Create fetchApi.js

---
+++ functions/fetchApi.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const baseAPIUrl = "http://animek.eu-4.evennode.com/"
+
+const searchAnime = async (query) => {
+  try {
+    const request = await axios.get(`${baseAPIUrl}search/${query}`)
+    const response = request.data
+
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { baseAPIUrl, searchAnime }
