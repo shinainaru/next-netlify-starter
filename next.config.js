1c87a1109bc92ae16b810fad21c50fb8fc4d5fc9
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-04-01 15:53:03 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.myanimelist.net"],
+    domains: ["cdn.myanimelist.net", "api.yimian.xyz"],
   },
   future: {
     webpack5: true
