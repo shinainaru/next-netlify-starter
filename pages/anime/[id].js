277fb5bb4718211b3e76a38cc6164f83377afaff
shinainaru <90768067+shinainaru@users.noreply.github.com>
2022-04-20 20:18:38 +0700

    Update [id].js

--- pages/anime/[id].js
+++ pages/anime/[id].js
@@ -1,1 +1,19 @@
 
+import Head from 'next/head'
+import Header from '@components/Header'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Mr.Cyser#R00t</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Mr.Cyser#R00t like hentai" />
+        Gw dah kena mental sosmed anjing.
+      </main>
+    </div>
+  )
+}
